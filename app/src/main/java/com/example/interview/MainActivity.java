1a4cf57d09297b028cc931a6763d47879eec15fe
liuqi <liuqi2455@126.com>
2022-02-09 17:30:04 +0800

    Loper TEST

--- app/src/main/java/com/example/interview/MainActivity.java
+++ app/src/main/java/com/example/interview/MainActivity.java
@@ -3,10 +3,16 @@ package com.example.interview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 
 import com.example.interview.ui.main.MainFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private Handler handler=new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +22,28 @@ public class MainActivity extends AppCompatActivity {
             getSupportFragmentManager().beginTransaction()
                     .replace(R.id.container, MainFragment.newInstance())
                     .commitNow();
+        }
+
+        LooperThread thread=new LooperThread();
+        thread.run();
+
+        
+    }
+
+    class LooperThread extends Thread {
+        public Handler mHandler;
+
+        public void run() {
+
+            if (Looper.myLooper()==null)
+                Looper.prepare();
+            mHandler = new Handler(Looper.myLooper()) {
+                public void handleMessage(Message msg) {
+                    // process incoming messages here
+                }
+            };
+
+            Looper.loop();
         }
     }
 }
